7807530780e254c40b1e4b85c771542fcdff93ce
PranavAGautm007 <64642668+PranavAGautm007@users.noreply.github.com>
2020-06-17 16:08:39 +0530

    Add files via upload

---
+++ circle.js
@@ -0,0 +1,22 @@
+class Circle{
+    constructor(x, y, radius ) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.circle(x, y, radius, options);
+        this.radius=radius;
+        this.image = loadImage("polygon.png");
+        World.add(world, this.body);
+      }
+      display(){
+
+        push();
+        translate(this.body.position.x, this.body.position.y);
+       
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.radius);
+        pop();
+      }
+}
